fb1ca39eeaf23fb339f9d793ba61e21de856e665
JaykumarZebra <jaykumar.gupta@zebra.com>
2026-01-23 14:36:56 +0530

    Exception Handler added

---
+++ common/src/test/java/com/jayzebra/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayzebra.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
